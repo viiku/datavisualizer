ffab47d65ffe2e4cac75de5fd26998edfa09cb90
viiku <vivekvikkusingh@gmail.com>
2025-05-02 20:04:12 +0530

    clean visualizer files

--- src/main/java/com/viiku/visualizer/service/VisualizerService.java
+++
@@ -1,9 +0,0 @@
-package com.viiku.visualizer.service;
-
-/**
- * Interface named {@link VisualizerService} should implement
- * multiple methods related to various business logic
- */
-
-public interface VisualizerService {
-}
--- src/main/java/com/viiku/visualizer/service/impl/VisualizerServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package com.viiku.visualizer.service.impl;
-
-import com.viiku.visualizer.service.VisualizerService;
-
-public class VisualizerServiceImpl implements VisualizerService {
-}
